f56504c76e713ad94a6878068862023a20bc8446
unknown <sashardoval@gmail.com>
2023-02-25 15:08:21 -0800

    inquirer debug'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateLogo = require('./lib/shapes.js'); //logo generator
+const generateLogo = require('./lib/shapes'); //logo generator
 
 // Create an array of questions for user input
 const questions = [{
@@ -26,23 +26,6 @@ const questions = [{
 }
 ];
 
-function makeShape(logoData) {
-    const { text, textColor, shape, shapeColor } = logoData;
-
-    //create svg parameters in .svg format
-    const svgText = `
-<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-    <${shape} cx="150" cy="100" r="80" fill="${shapeColor}" />
-
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-
-</svg>`;
-
-
-    // return svg code
-    return svgText;
-}
 
 //Create a function to initialize logo
 function init() {
@@ -50,10 +33,10 @@ function init() {
         .then((logoData) => {
 
             //spits out user answers from shapes function
-            const returnedSvg = makeShape(logoData);
+            const returnedSvg = generateLogo(logoData);
 
             // TODO: Create a function to write README file
-            fs.writeFile('logo.svg', returnedSvg, (err) =>
+            fs.writeFile('./examples/logo.svg', returnedSvg, (err) =>
                 err ? console.log(err) : console.log('Generated logo.svg')
             );
         });
