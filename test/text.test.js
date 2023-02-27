1b399386d902b3f80b74be20b396c89544c8116b
unknown <sashardoval@gmail.com>
2023-02-27 15:49:58 -0800

    correct svg shapes

---
+++ test/text.test.js
@@ -0,0 +1,11 @@
+const Text = require('../lib/text.js');
+//const { formatDate } = require('../lib/date.js');
+
+describe('Text', () => {
+  test('Should not be more than 3 characters', () => {
+    const expectedSvg = 
+      `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
+    const text = new Text();
+    expect(text.render()).toEqual(expectedSvg);
+  });
+});
